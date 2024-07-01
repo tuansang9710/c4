06f9b16c893cc4980d62a188965f60e5c5349ce8
Tran Tuan Sang <tuansang9710@gmail.com>
2024-07-01 21:06:12 +0700

    add connectionStr
    add connectionStr

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -18,7 +18,7 @@ from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 
-appInsightConnectionStr = os.environ.get('AppInsightConnectionString')
+appInsightConnectionStr = os.environ.get('AppInsightConnectionString') or 'InstrumentationKey=61537851-56f6-4f0c-a3ed-9a34324144d6;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=43d92f0c-f0c2-4001-9c96-b0bfc83ea750'
 
 # Logging
 logger = logging.getLogger(__name__)
